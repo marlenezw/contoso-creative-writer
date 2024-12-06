af928f8f96aff0d80ccd771d3a8411deb16352eb
Marlene <57748216+marlenezw@users.noreply.github.com>
2024-12-06 19:08:16 +0000

    Configure Azure Developer Pipeline

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -39,9 +39,9 @@ echo "--- ✅ | 1. Post-provisioning - env configured ---"
 
 # Setup to run notebooks
 echo 'Installing dependencies from "requirements.txt"'
-python -m pip install -r src/api/requirements.txt > /dev/null
-python -m pip install ipython ipykernel > /dev/null      # Install ipython and ipykernel
-ipython kernel install --name=python3 --user > /dev/null # Configure the IPython kernel
+python3 -m pip install -r src/api/requirements.txt 
+python3 -m pip install ipython ipykernel       # Install ipython and ipykernel
+ipython kernel install --name=python3 --user  # Configure the IPython kernel
 jupyter kernelspec list > /dev/null                      # Verify kernelspec list isn't empty
 echo "--- ✅ | 2. Post-provisioning - ready execute notebooks ---"
 
